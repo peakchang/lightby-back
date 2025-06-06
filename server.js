e1441c64356cd4a6f43fcdf79f1c97daa4986f8d
park changyong <ridebus82@nate.com>
2025-06-06 11:17:02 +0900

    250606 asldfjogogogogo

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ app.get('/chkserver', (req, res) => {
 
 app.listen(app.get('port'), () => {
     console.log('?!?!?!?!?!??!?!?!');
-    
+
     console.log(`server running in port ${app.get('port')}`);
 })
