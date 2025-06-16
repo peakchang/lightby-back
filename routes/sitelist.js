b244d1cef2cb4c7dcb526d7f8edaa0422976aa9a
rider <ridebus82@nate.com>
2025-06-16 15:52:26 +0900

    250616 asdfljgogogo

--- routes/sitelist.js
+++ routes/sitelist.js
@@ -9,18 +9,33 @@ const sitelistRouter = express.Router();
 sitelistRouter.post('/load_site_list', async (req, res, next) => {
     console.log('chk');
 
+    const { mainLocation } = req.body
+    console.log(mainLocation);
+    
+
+
+    let premium_list = [];
+    let top_list = [];
     let site_list = [];
 
     const rows = "idx, thumbnail,imgs,subject,point,fee_type,fee,business,occupation"
     try {
-        const getSiteListQuery = `SELECT ${rows} FROM site ORDER BY idx DESC`
-        const [getSiteList] = await sql_con.promise().query(getSiteListQuery);
+        const getPremiumListQuery = `SELECT ${rows} FROM site WHERE product = ? ORDER BY idx DESC`
+        const [getPremiumList] = await sql_con.promise().query(getPremiumListQuery, ['premium']);
+        premium_list = getPremiumList
+
+        const getTopListQuery = `SELECT ${rows} FROM site WHERE product = ? ORDER BY idx DESC`
+        const [getTopList] = await sql_con.promise().query(getTopListQuery, ['top']);
+        top_list = getTopList
+
+        const getSiteListQuery = `SELECT ${rows} FROM site WHERE (product = ? OR product IS NULL OR product = '') ORDER BY idx DESC`
+        const [getSiteList] = await sql_con.promise().query(getSiteListQuery, ['free']);
         site_list = getSiteList
     } catch (error) {
 
     }
 
-    res.json({ site_list })
+    res.json({ premium_list, top_list, site_list })
 })
 
 export { sitelistRouter }
