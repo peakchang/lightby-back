f5fb30485c4686d9dd2df1c07fd4e73d5865efb9
rider <ridebus82@nate.com>
2025-06-20 13:15:54 +0900

    250620 asdfljogogogo

--- routes/sitelist.js
+++ routes/sitelist.js
@@ -7,31 +7,53 @@ import qs from 'qs'
 const sitelistRouter = express.Router();
 
 sitelistRouter.post('/load_site_list', async (req, res, next) => {
+
     console.log('chk');
 
     const { mainLocation } = req.body
     console.log(mainLocation);
-    
+
+    let locationQueryStr = ""
+    if (mainLocation && mainLocation != '전국') {
+        const locationList = mainLocation.split('/')
+        for (let i = 0; i < locationList.length; i++) {
+            const str = locationList[i];
+            if (i == 0) {
+                locationQueryStr = locationQueryStr + ` AND location LIKE '%${str}%'`
+            } else {
+                locationQueryStr = locationQueryStr + ` OR location LIKE '%${str}%'`
+            }
+        }
+    }
+
+    console.log(locationQueryStr);
 
 
     let premium_list = [];
     let top_list = [];
     let site_list = [];
 
-    const rows = "idx, thumbnail,imgs,subject,point,fee_type,fee,business,occupation,icons"
+    const rows = "idx,location,thumbnail,imgs,subject,point,fee_type,fee,business,occupation,icons"
     try {
-        const getPremiumListQuery = `SELECT ${rows} FROM site WHERE product = ? ORDER BY idx DESC`
+        const getPremiumListQuery = `SELECT ${rows} FROM site WHERE product = ? ${locationQueryStr} ORDER BY idx DESC`
+
+        console.log(getPremiumListQuery);
+
         const [getPremiumList] = await sql_con.promise().query(getPremiumListQuery, ['premium']);
         premium_list = getPremiumList
 
-        const getTopListQuery = `SELECT ${rows} FROM site WHERE product = ? ORDER BY idx DESC`
+        console.log(premium_list);
+        
+
+        const getTopListQuery = `SELECT ${rows} FROM site WHERE product = ? ${locationQueryStr} ORDER BY idx DESC`
         const [getTopList] = await sql_con.promise().query(getTopListQuery, ['top']);
         top_list = getTopList
 
-        const getSiteListQuery = `SELECT ${rows} FROM site WHERE (product = ? OR product IS NULL OR product = '') ORDER BY idx DESC`
+        const getSiteListQuery = `SELECT ${rows} FROM site WHERE (product = ? OR product IS NULL OR product = '') ${locationQueryStr} ORDER BY idx DESC`
         const [getSiteList] = await sql_con.promise().query(getSiteListQuery, ['free']);
         site_list = getSiteList
     } catch (error) {
+        console.error(error.message);
 
     }
 
